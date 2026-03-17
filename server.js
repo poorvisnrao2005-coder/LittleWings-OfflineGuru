6da24389fba6076a2422ddf5a0d6e10746c32d6d
poorvisnrao2005-coder <poorvisnrao2005@gmail.com>
2026-03-17 11:16:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(cors());
+app.use(express.json());
+
+// Serve static frontend files
+app.use(express.static(__dirname));
+
+// Home route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
